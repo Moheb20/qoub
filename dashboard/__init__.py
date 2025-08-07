0a302090fe546cc5e3d6006c9de3ed5b97fe19c8
Moheb20 <moheb.alwehwah@gmail.com>
2025-08-07 12:40:57 +0300

    Delete dashboard directory

--- dashboard/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from dashboard.routes import dashboard_bp
-
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = 'your-super-secret-key'
-    app.register_blueprint(dashboard_bp)
-    return app
