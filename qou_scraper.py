3666a58e662afa2bc585e5ba4fc88d7fd6813dbb
Moheb20 <moheb.alwehwah@gmail.com>
2025-08-15 14:07:44 +0300

    Update qou_scraper.py

--- qou_scraper.py
+++ qou_scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from typing import Optional, List
+from datetime import datetime
 
 LOGIN_URL = 'https://portal.qou.edu/login.do'
 INBOX_URL = 'https://portal.qou.edu/student/inbox.do'
