840652477e3de39f264e782d6f6f1683ddc54755
Moheb20 <moheb.alwehwah@gmail.com>
2025-09-15 15:05:38 +0300

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,1 +1,13 @@
+from .admin_handlers import setup_admin_handlers
+from .user_handlers import setup_user_handlers
+from .contact_handlers import setup_contact_handlers
+from .deadline_handlers import setup_deadline_handlers
+from .group_handlers import setup_group_handlers
 
+__all__ = [
+    'setup_admin_handlers',
+    'setup_user_handlers', 
+    'setup_contact_handlers',
+    'setup_deadline_handlers',
+    'setup_group_handlers'
+]
