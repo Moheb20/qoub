4d2cdbef12b6135c26af0008778a4f5890a708ee
Moheb20 <moheb.alwehwah@gmail.com>
2025-09-17 14:49:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def send_main_menu(chat_id):
         markup.add(types.KeyboardButton("📖 الخدمات الأكاديمية"))
         markup.add(types.KeyboardButton("📅 التقويم الحالي"))
         current_week_text = QOUScraper.get_current_week_type()
-        markup.add(types.KeyboardButton(current_week_text))
+        markup.add(types.InlineKeyboardButton(current_week_text, callback_data="noop"))
         markup.add(types.KeyboardButton("📚 أخرى"))
         markup.add(types.KeyboardButton("🚪 تسجيل الخروج"))
         if chat_id in ADMIN_CHAT_ID:
