4bb734f1d85bcf1e39fc0428eca3aebd3ad7d815
Moheb20 <moheb.alwehwah@gmail.com>
2025-09-19 22:43:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1430,7 +1430,7 @@ def handle_all_messages(message):
             return
         
         # استدعاء دالة السكرابنق الجديدة
-        portal_data = fetch_student_data_from_portal(creds['username'], creds['password'])
+        portal_data = scraper.fetch_student_data_from_portal(creds['username'], creds['password'])
         
         # معالجة النتيجة
         if portal_data["success"]:
