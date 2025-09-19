2f1aef71d2e8472c54196a1b1fb043218023d6d0
Moheb20 <moheb.alwehwah@gmail.com>
2025-09-19 22:57:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1432,7 +1432,7 @@ def handle_all_messages(message):
         # إنشاء كائن سكرابر جديد - هذا هو التصحيح المهم!
             scraper = QOUScraper(creds['username'], creds['password'])
         # استدعاء دالة السكرابنق الجديدة
-            portal_data = scraper.fetch_student_data_from_portal(creds['username'], creds['password'])
+            portal_data = fetch_student_data_from_portal(creds['username'], creds['password'])
         
         # معالجة النتيجة
         if portal_data["success"]:
